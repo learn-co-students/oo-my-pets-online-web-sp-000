21357c308bd439a5b24b10d0a90cfc65f1f339b1
Austin  Burke <aburk3@gmail.com>
2018-10-15 20:45:54 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,34 @@
+require 'pry'
 class Owner
-  
+  attr_reader :species
+  attr_accessor :name, :pets
+
   @@all = []
-  
-  def initialize(owner)
-    @@all << owner
+
+  def initialize(species)
+    @@all << self
+    @species = species
+    @pets = {fishes: [], dogs: [], cats: []}
   end
-  
+
   def self.all
-    @all
+    @@all
+    # binding.pry
   end
-  
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_fish(name)
+  end
+
 end
