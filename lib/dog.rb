41b0cfed757848ff8b3ddfe8370792d2a1809219
Austin  Burke <aburk3@gmail.com>
2018-10-16 11:03:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,10 @@
 class Dog
   attr_accessor :mood
   attr_reader :name
-  
+
   def initialize(pet_name)
     @name = pet_name
     @mood = "nervous"
   end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -32,4 +32,51 @@ class Owner
     pets[:fishes] << Fish.new(name)
   end
 
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      # binding.pry
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cats|
+      cats.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    pets.each do |species, pet|
+      pet.each do |animal|
+        animal.mood = "nervous"
+      end
+    pet.clear
+    end
+  end
+
+  def list_pets
+    # binding.pry
+    fish = pets[:fishes].size
+    dogs = pets[:dogs].size
+    cats = pets[:cats].size
+
+    "I have #{fish} fish, #{dogs} dog(s), and #{cats} cat(s)."
+  end
+
+
+
 end
