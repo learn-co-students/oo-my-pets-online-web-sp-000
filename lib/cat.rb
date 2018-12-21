194e17c668e5655a98f282d842494f594dc11eb2
Ariel Ordonez <arielord@buffalo.edu>
2018-12-21 17:37:45 -0500

    dog

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,6 @@ class Cat
   # code goes here
   attr_accessor :mood, :name
 
-
   def initialize(name, mood = "nervous")
     self.name = name
   end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,8 @@
 class Dog
   # code goes here
+  attr_accessor :mood, :name
+
+  def initialize(name, mood = "nervous")
+    self.name = name
+  end
 end
