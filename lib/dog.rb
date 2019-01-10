f7c4e48df02c1a27f141f0d04527251cf01879a3
karen ercoli <karenercoli@gmail.com>
2019-01-10 23:18:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
+  attr_reader :name
+  attr_accessor :mood
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def mood
+    @mood
+  end
   # code goes here
 end
