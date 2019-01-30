8a60dd01182d3ef8b03ed3c4288fa309a8c8806f
J. Singleton <jennifer.singleton@gmail.com>
2019-01-30 16:23:36 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,78 @@
+require 'pry'
 class Owner
   # code goes here
+  attr_accessor :pets, :name
+
+  @@all = []
+
+  def initialize(species)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+    @species = species
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[:cats] << new_cat
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
+
+  def change_mood(animal, mood)
+    @pets[animal].each do |pet|
+      pet.mood = mood
+    end
+  end
+
+  def walk_dogs
+    change_mood(:dogs, "happy")
+  end
+
+  def play_with_cats
+    change_mood(:cats, "happy")
+  end
+
+  def feed_fish
+    change_mood(:fishes, "happy")
+  end
+
+  def sell_pets
+    change_mood(:dogs, "nervous")
+    change_mood(:cats, "nervous")
+    change_mood(:fishes, "nervous")
+    @pets.clear
+  end
+
 end
