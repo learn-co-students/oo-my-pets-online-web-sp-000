557afdb2c7d5f57eb3928b8213ed2cbf9d209d98
Abagail Hudson <abagailanne@gmail.com>
2019-05-29 00:05:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
-  # code goes here
+
+  attr_reader :name #use a reader so non purposeful name modifications can occur
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
 end
