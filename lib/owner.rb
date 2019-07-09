b11e16f91b90fac1aef8b178f2ede908c36c48f2
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-09 21:14:23 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,23 +1,33 @@
 class Owner
 
 @@all = []
-def initialize(species)
-  @name
-  @species = species
-  @species.freeze
+def initialize(name)
+  @name = name
+  @name.freeze
   @@all << self
+  @species = "human"
+  # @species = species
+  # @species.freeze
+  # @@all << self
   # @name = name
   # @name.freeze
   # @@all << self
 end
-def name=(name)
-  @name = name
-  @name.freeze
-  @@all << self
+# def name=(name)
+#   @name = name
+#   @name.freeze
+#   @@all << self
+# end
+def name
+  @name
 end
 def species
   @species
 end
+def say_species
+   "I am a #{@species}."
+end
+
 def self.all
   @@all
 end
@@ -26,6 +36,9 @@ def self.count
 end
 def self.reset_all
   @@all.clear
+end
+def buy_cat
+  Cat.new
 end
 
 
