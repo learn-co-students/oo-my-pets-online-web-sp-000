0a5c3cdc0a97875cc5ce7ba028f2ad97f0750415
Isai Bismark Cortez <isai0505@gmail.com>
2019-09-01 04:28:00 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,47 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :dogs, :cats
+  attr_reader :species, :name
+
+  @@all = []
+  @@count = 0
+  @@pets = {:dogs => [], :cats => []}
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @@count += 1
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.reset_all
+    @@count = 0
+    @@all.clear
+  end
+
+  def pets
+    @@pets
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+   def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
 end
