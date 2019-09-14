2d02b131e5c9c4dafec2041aaea4f3f0c058f5a5
Panela Leung <leungpanela@gmail.com>
2019-09-14 19:54:40 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+  def initialize (name, owner)
+    @name = name # Crookshanks
+    @owner = owner # Hermione
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
+  attr_reader :name, :owner, :species
+
+  @@all= []
+
+  def initialize(name)
+    @name = name
+    @owner = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+ #class methods
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
